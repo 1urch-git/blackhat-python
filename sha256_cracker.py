dfb3de386a9b9d7934e6c7374772cdd982a519eb
1urch <charleshreed10@gmail.com>
2023-01-20 00:32:58 -0500

    Update sha256_cracker.py

--- sha256_cracker.py
+++ sha256_cracker.py
@@ -4,18 +4,20 @@ import sys
 
 if len(sys.argv) != 2:
   print("Invalid number of arguments")
-  print(">> {} <sha256sum>".format(sysargv[0]))
+  print(">> {} <sha256sum> <wordlist>".format(sysargv[0]))
   exit()
 
   wanted_hash = sys.argv[1]
   print(wanted_hash)
-  password_file = "rockyou.txt"
+  password_file = sys.argv[2]
+  #"/usr/share/wordlists/rockyou.txt" as an example
   attempts = 0
   
   with log.progress("Attempting: {}!\n".format(wanted_hash)) as p:
       with open(password_file, "r", encoding='latin-1') as password_list:
           for password in password_file:
               password = password.strip("\n").encode('latin-1')
+              #strip newline character from password file so it is not included in hashing operation
               password_hash = sha256sumhex(password)
               p.status("[{}] {} == {}".format(attempts, password.decode('latin-1'), password_hash)
               if password_hash == wanted_hash:
