b13a1f94213423a489229a27940f7bf9ec9aee51
1urch <charleshreed10@gmail.com>
2023-01-20 00:27:28 -0500

    Update sha256_cracker.py

--- sha256_cracker.py
+++ sha256_cracker.py
@@ -6,3 +6,21 @@ if len(sys.argv) != 2:
   print("Invalid number of arguments")
   print(">> {} <sha256sum>".format(sysargv[0]))
   exit()
+
+  wanted_hash = sys.argv[1]
+  print(wanted_hash)
+  password_file = "rockyou.txt"
+  attempts = 0
+  
+  with log.progress("Attempting: {}!\n".format(wanted_hash)) as p:
+      with open(password_file, "r", encoding='latin-1') as password_list:
+          for password in password_file:
+              password = password.strip("\n").encode('latin-1')
+              password_hash = sha256sumhex(password)
+              p.status("[{}] {} == {}".format(attempts, password.decode('latin-1'), password_hash)
+              if password_hash == wanted_hash:
+                  print("password")
+              attempts += 1
+          p.failure("Password not found")
+  
+  
